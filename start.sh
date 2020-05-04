e1f12256e8079030980bd3d4a912304aded27beb
emily <emileet@plsnobully.me>
2020-05-04 19:05:55 +1000

    cleanup start script

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 # starts a arch-systemd container
 
 if [[ ! -z $1 ]]; then
-    sudo docker run -ti --privileged=true --name $1 -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /mnt/docker/setup:/setup:rw -p 25591:25565 -p 25590:9090 -d emileet/arch-systemd
+    sudo docker run -ti --privileged=true --name $1 -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 8080:80 -d emileet/arch-systemd
 else
     echo "please provide a container name"
 fi
